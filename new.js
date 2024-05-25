9d41e748af2f9b34f2e72e9f910e5643e8b987d5
Vlad <seven712234234@gmail.com>
2024-05-25 11:33:12 +0300

    added improved version with browser

--- new.js
+++ new.js
@@ -1,18 +1,14 @@
-var prompt = require('prompt');
+var year = prompt();
+year = Number(year)
 
-prompt.start();
-prompt.get(['year'], function (err, result) {
-  year = Number(result.year)
-
-  if(year && year > 0){
-    if(year === 1){
-      console.log(year, " рік");
-    } else if(year > 1 && year < 5){
-      console.log(year, " роки");
-    } else if(year >= 5){
-      console.log(year, " років");
-    }
-  } else {
-    console.log("wrong number");
+if(year && year > 0){
+  if(year === 1){
+    console.log(year, "рік");
+  } else if(year > 1 && year < 5){
+    console.log(year, "роки");
+  } else if(year >= 5){
+    console.log(year, "років");
   }
-});
+} else {
+  console.log("wrong number");
+}
